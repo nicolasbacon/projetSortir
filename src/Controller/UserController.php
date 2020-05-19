787846a723d0f11cf6bc7611a476feb758b508f4
kaktus-elver <urbman78@hotmail.fr>
2020-05-19 14:09:53 +0200

    register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    /**
+     * @Route("/register" name="user_register")
+     */
+    public function register(Request $request, EntityManagerInterface $em)
+    {
+        $user = new User();
+        $registerForm = $this->createForm(RegisterType::class);
+
+        $registerForm->handleRequest($request);
+
+        if ($registerForm->isValid() && $registerForm->isSubmitted()) {
+
+        }
+        return $this->render('user/register.html.twig', []);
+    }
+
     /**
      * @Route("/login", name="login")
      */
@@ -18,7 +38,7 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name=""logout")
+     * @Route("/logout", name="logout")
      */
     public function logout(){}
 }
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('mail', EmailType::class)
+            ->add('motPasse', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+            ->add('administrateur')
+            ->add('actif')
+            ->add('campus')
+            ->add('sorties')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
