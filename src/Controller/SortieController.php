91b37edee6a51b28dabced05407bf378f49a3c6e
delphetitoun <delphine.omilli35@gmail.com>
2020-05-19 10:54:14 +0200

    Creation sortie

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    /**
+     * @Route("/sortie", name="sortie")
+     */
+    public function index()
+    {
+        return $this->render('sortie.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/add", name="add_sortie")
+     */
+    public function addSortie(EntityManagerInterface $em, Request $request)
+    {
+        //@todo : traiter le formulaire
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        $sortieForm->handleRequest($request);
+
+        if($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            $em->persist($sortie);
+            $em->flush();
+            $this->addFlash('success', 'La sortie a été ajoutée !');
+            return $this->redirectToRoute('add_sortie');
+        }
+        return $this->render('sortie/add.html.twig', [
+            'sortieForm' => $sortieForm->createView()
+        ]);
+    }
+}
