1865dae5fcca325f9e6574476b94e127455cdcff
kaktus-elver <urbman78@hotmail.fr>
2020-05-20 15:40:56 +0200

    tableau

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +12,14 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(EntityManagerInterface $em)
     {
+        $sorties = $em->getRepository(Sortie::class)->findAll();
+
+
         return $this->render('main/accueil.html.twig', [
             'controller_name' => 'MainController',
+            "sorties" => $sorties,
         ]);
     }
 }
