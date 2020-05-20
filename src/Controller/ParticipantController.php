7dce1a60f7b5d8075ebd2b753bac14460668c319
delphetitoun <delphine.omilli35@gmail.com>
2020-05-20 10:08:29 +0200

    Modif relation sortie

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -17,8 +17,7 @@ class ParticipantController extends AbstractController
      * @Route("/modifier", name="participant_modifier")
      * @param Request $request
      * @param EntityManagerInterface $em
-
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+ * @return \Symfony\Component\HttpFoundation\Response
      */
     public function modifierProfil(
        UserPasswordEncoderInterface $passwordEncoder, Request $request, EntityManagerInterface $em)
