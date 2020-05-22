74e624b0259f4b7d442a0c61843f815a72c20198
kaktus-elver <urbman78@hotmail.fr>
2020-05-22 17:04:59 +0200

    css afficher:modifier profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Sortie;
+use App\Entity\Campus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +15,15 @@ class MainController extends AbstractController
      */
     public function home(EntityManagerInterface $em)
     {
-        $user = $this->getUser();
         $sortieRepo = $em->getRepository(Sortie::class);
         $campusRepo = $em->getRepository(Campus::class);
-
-        $sorties = $sortieRepo->findByCampus($user->getCampus()->getId());
+        $user = $this->getUser();
+        if ($user != null) {
+            $sorties = $sortieRepo->findByCampus($user->getCampus()->getId());
+        }
+       else{
+           $sorties = $sortieRepo->findAll();
+       }
         $allCampus = $campusRepo->findAll();
 
         return $this->render('main/accueil.html.twig', [
