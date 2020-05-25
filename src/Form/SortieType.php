4995d818c36a46f35a4c64bfd5ab363ddf8dda0e
delphetitoun <delphine.omilli35@gmail.com>
2020-05-25 16:03:00 +0200

    presentation sortie + création page annuler sortie et modifier sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -8,6 +8,7 @@ use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -22,7 +23,7 @@ class SortieType extends AbstractType
         $builder
             ->add('nom', TextType::class)
             ->add('dateHeureDebut', DateTimeType::class)
-            ->add('dateLimiteInscription', DateTimeType::class)
+            ->add('dateLimiteInscription', DateType::class)
             ->add('nbInscriptionsMax', IntegerType::class)
             ->add('duree', TimeType::class)
             ->add('infosSortie', TextareaType::class)
