3fd4e15983f7a27eaf82cb3316cf598fa1e1241f
kaktus-elver <urbman78@hotmail.fr>
2020-05-27 12:49:24 +0200

    un peu de css sur accueil add sortie et login

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -122,6 +122,7 @@ class SortieController extends AbstractController
     {
         //récupérer la sortie en BDD:
         $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
+        //récuperer l'état en BDD
         $etatRepository = $em->getRepository(Etat::class);
 
         $sortie = $sortieRepository->find($id);
@@ -170,7 +171,9 @@ class SortieController extends AbstractController
     {
         //récupérer la sortie en BDD:
         $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
+        $etatRepository = $em->getRepository(Etat::class);
         $sortie = $sortieRepository->find($id);
+
 
         $sortieAnulForm = $this->createForm(SortieAnnuleeType::class, $sortie);
         $sortieAnulForm->handleRequest($request);
@@ -180,9 +183,7 @@ class SortieController extends AbstractController
         }
         else {
             if($sortieAnulForm->isSubmitted() && $sortieAnulForm->isValid()) {
-                $etat = new Etat();
-                $etat->setLibelle('Annulee');
-                $sortie->setEtat($etat);
+                $sortie->setEtat($etatRepository->find(5));
 
                 $em->persist($sortie);
                 $em->flush();
@@ -194,5 +195,7 @@ class SortieController extends AbstractController
             'sortieAnulForm' => $sortieAnulForm->createView(),
         ]);
     }
+
+
 
 }
